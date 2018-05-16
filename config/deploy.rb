873e0315f65d0479e1a7b9a9b74bf30ffbd26ba2
Joshua Madin <jmadin@hawaii.edu>
2018-05-15 16:26:10 -1000

    Deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,4 +40,4 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bund
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
+#set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
