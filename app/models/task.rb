08afc32994b3b89cca1a1881efe1f2235759c760
Joshua Madin <jmadin@hawaii.edu>
2018-05-25 14:49:04 -1000

    Sunspot.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,17 +1,17 @@
 class Task < ApplicationRecord
 
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+  # include Elasticsearch::Model
+  # include Elasticsearch::Model::Callbacks
 
-  settings do
-    mappings dynamic: false do
-      indexes :name, type: :text, analyzer: :english
-      indexes :finished, type: :boolean
-    end
-  end
-
-  # searchable do
-  #   text :name
+  # settings do
+  #   mappings dynamic: false do
+  #     indexes :name, type: :text, analyzer: :english
+  #     indexes :finished, type: :boolean
+  #   end
   # end
+
+  searchable do
+    text :name
+  end
 
 end
